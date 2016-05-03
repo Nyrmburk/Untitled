ee6855764c6a6e7aa2b2cfe4d66767c304dded33
Christopher Dombroski <dombroski.christopher@gmail.com>
2016-05-03 13:26:35 -0700

    Add initial documentation for modelconverter.Line

--- src/graphics/modelconverter/Line.java
+++ src/graphics/modelconverter/Line.java
@@ -5,6 +5,13 @@ import main.Vec2;
 
 /**
  * Created by Nyrmburk on 5/3/2016.
+ *
+ * Requirements for line rendering:
+ * 1. Varying line thickness
+ * 2. Varying color
+ * 3. Miter and bevel edges
+ * 4. Looping (add edges to line if end == start)
+ * 5. antialiased?
  */
 public class Line implements ModelConverter<Vec2[]> {
 
