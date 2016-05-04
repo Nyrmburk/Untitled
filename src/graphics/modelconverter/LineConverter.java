ed07a95b7db0799b78baf86ed2a16b5cb1270b1f
Christopher Dombroski <dombroski.christopher@gmail.com>
2016-05-04 00:05:27 -0700

    Work on LineConverter
    Also beefed up Vec2 a bit by adding transpose() and changing how
    normalize() worked.

--- src/graphics/modelconverter/LineConverter.java
+++ src/graphics/modelconverter/LineConverter.java
@@ -6,21 +6,23 @@ import main.Vec2;
 
 /**
  * Created by Nyrmburk on 5/3/2016.
- *
+ * <p>
  * Requirements for line rendering:
  * 1. Varying line thickness
  * 2. Varying color
  * 3. Miter and bevel edges
  * 4. Looping (add edges to line if end == start)
  * 5. antialiased?
- *
+ * <p>
  * Implementation.
- *
- * The implementation I am taking is getting two line segments, normalizing them, and multiplying the length (1) by the
+ * <p>
+ * The implementation I am taking is getting two line segments, calculating the normal,normalizing them, and multiplying the length (1) by the
  * thickness. Next, add the vectors together to get the first vertex. It will be relative so we also need to add the
  * vertex position. To get the other side of the line, simply reverse the previous vector and again add the vertex.
  * I might do a test on the two lines to determine if they are an obtuse or acute angle. Then take the obtuse side and
  * bevel instead of miter if the length is too great relative to the thickness.
+ * <p>
+ * https://forum.libcinder.org/topic/smooth-thick-lines-using-geometry-shader#23286000001269127
  */
 public class LineConverter implements ModelConverter<Line> {
 
@@ -28,9 +30,26 @@ public class LineConverter implements ModelConverter<Line> {
 
 		ModelLoader model = new ModelLoader();
 
-		for (int i = 0; i < line.getLength(); i++) {
+		Vec2[] verts = line.getVertices();
 
+		Vec2 inLine = verts[1].subtract(verts[0]).normalized();
 
+		for (int i = 1; i < line.getLength(); i++) {
+
+			float width = line.getWidth()[i] / 2;
+
+			Vec2 outLine = verts[i + 1].subtract(verts[i]).normalized();
+			Vec2 normal = outLine.transpose();
+			Vec2 tangent = inLine.add(outLine).normalized();
+
+			Vec2 miter = new Vec2(-tangent.getY(), tangent.getX());
+			float length = width / miter.dot(normal);
+			miter = miter.normalized().multiply(length);
+
+			Vec2 leftVert = verts[i].add(miter);
+			Vec2 rightVert = verts[i].subtract(miter);
+
+			inLine = outLine;
 		}
 
 		return model;
--- src/main/Vec2.java
+++ src/main/Vec2.java
@@ -1,127 +1,137 @@
 package main;
 
 public class Vec2 {
-	
+
 	private float x;
 	private float y;
-	
+
 	public Vec2() {
 	}
-	
+
 	public Vec2(float x, float y) {
-		
+
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public Vec2(Vec2 copy) {
-		
+
 		this(copy.x, copy.y);
 	}
-	
+
 	public float getX() {
-		
+
 		return x;
 	}
-	
+
 	public float getY() {
-		
+
 		return y;
 	}
-	
+
 	public Vec2 add(Vec2 toAdd) {
-		
+
 		return new Vec2(x + toAdd.x, y + toAdd.y);
 	}
-	
+
 	public Vec2 subtract(Vec2 toSub) {
-		
+
 		return new Vec2(x - toSub.x, y - toSub.y);
 	}
-	
+
 	public Vec2 multiply(Vec2 toMult) {
-		
+
 		return new Vec2(x * toMult.x, y * toMult.y);
 	}
-	
+
 	public Vec2 multiply(float scalar) {
-		
+
 		return new Vec2(x * scalar, y * scalar);
 	}
-	
+
 	public Vec2 divide(Vec2 toDiv) {
-		
+
 		return new Vec2(x / toDiv.x, y / toDiv.y);
 	}
-	
+
 	public Vec2 divide(float scalar) {
-		
+
 		return new Vec2(x / scalar, y / scalar);
 	}
-	
-	public Vec2 invert() {
-		
-		return new Vec2(1/x, 1/y);
+
+	public float dot(Vec2 toDot) {
+
+		return x * toDot.x + this.y * toDot.y;
 	}
-	
+
+	public Vec2 transpose() {
+
+		return new Vec2(-y, x);
+	}
+
+	public Vec2 invert() {
+
+		return new Vec2(1 / x, 1 / y);
+	}
+
 	public Vec2 negate() {
-		
+
 		return new Vec2(-x, -y);
 	}
-	
-	public void normalize() {
-		
-		setPosition(this.divide(distance()));
+
+	public Vec2 normalized() {
+
+		return new Vec2(this.divide(distance()));
 	}
-	
+
+
 	public void translate(Vec2 delta) {
-		
+
 		setPosition(this.add(delta));
 	}
-	
+
 	public void rotate(Vec2 center, float radians) {
-		
+
 		double angle = Math.atan2(y - center.getY(), x - center.getX());
 		angle += radians;
-		
+
 		float distance = this.distance(center);
-		
+
 		float x = (float) Math.cos(angle) * distance + center.getX();
 		float y = (float) Math.sin(angle) * distance + center.getY();
-		
+
 		setPosition(new Vec2(x, y));
 	}
-	
+
 	public void scale(Vec2 center, float amount) {
-		
-		Vec2 delta = this.subtract(center);
-		delta.normalize();
+
+		Vec2 delta = this.subtract(center).normalized();
 		delta.multiply(amount);
-		
+
 		setPosition(delta.add(center));
 	}
-	
+
 	public float distance(Vec2 vec) {
-		
+
 		float a = vec.x - x;
 		float b = vec.y - y;
 		return (float) Math.sqrt(a * a + b * b);
 	}
-	
+
 	public float distance() {
-		
+
 		return (float) Math.sqrt(x * x + y * y);
 	}
-	
+
 	public void setPosition(Vec2 vec) {
-		
+
 		x = vec.x;
 		y = vec.y;
 	}
-	
+
 	@Override
 	public String toString() {
-		
-		return "[" + x + ", " + y + "]"; 
+
+		return "[" + x + ", " + y + "]";
 	}
 }
